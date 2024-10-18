0699c3070831e97812b082585c5d7bd4f07b33d2
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-10-18 18:52:29 -0300

    display geocode precision

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import EncodingPlugin from 'encoding-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config) => {
+        config.plugins.push(
+            new EncodingPlugin({
+                encoding: "ISO-8859-1",
+            })
+        );
         config.module.rules.push({
             // make all files ending in .jsonl use the `jsonlines-loader`
             test: /\.jsonl$/,
