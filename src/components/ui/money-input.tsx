2385404331030baa3a21b3bfda77ab20c197fcbf
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-10-21 15:26:54 -0300

    Improves input filters UI

---
+++ src/components/ui/money-input.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useReducer } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+type TextInputProps = {
+    initialValue: number;
+    onChange: (newValue: number) => void;
+    label: string;
+};
+
+// Brazilian currency config
+const moneyFormatter = Intl.NumberFormat("pt-BR", {
+    currency: "BRL",
+    // currencyDisplay: "symbol",
+    // currencySign: "standard",
+    // style: "currency",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
+export default function MoneyInput(props: TextInputProps) {
+    const { onChange, initialValue } = props;
+
+    const initialFormattedValue = moneyFormatter.format(initialValue);
+
+    const [value, setValue] = useReducer((_: unknown, next: string) => {
+        const digits = next.replace(/\D/g, "");
+        return moneyFormatter.format(Number(digits) / 100);
+    }, initialFormattedValue);
+
+    function handleChange(formattedValue: string) {
+        const digits = formattedValue.replace(/\D/g, "");
+        const realValue = Number(digits) / 100;
+        onChange(realValue);
+    }
+
+    return (
+        <div>
+            <Label>{props.label}</Label>
+            <Input
+                type="text"
+                startAdornment="R$"
+                onChange={(ev) => {
+                    setValue(ev.target.value);
+                    handleChange(ev.target.value);
+                }}
+                value={value}
+            />
+        </div>
+    );
+}
