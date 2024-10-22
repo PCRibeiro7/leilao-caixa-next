49858018f6aa035a33aa1411ba3d3c59dfbd7a90
Pedro Ribeiro <pribeiro@extensiv.com>
2024-10-22 20:03:50 -0300

    removes webpack changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            // make all files ending in .jsonl use the `jsonlines-loader`
-            test: /\.jsonl$/,
-            use: "jsonlines-loader",
-            type: "javascript/auto",
-        });
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
