8e0af1d9562845bc3f84e567ab23e33c19e3a435
Pedro Ribeiro <pribeiro@extensiv.com>
2024-10-22 17:48:17 -0300

    converts supabase request to hook

--- src/pages/FilteredMap/index.tsx
+++ src/pages/FilteredMap/index.tsx
@@ -2,28 +2,13 @@
 
 import MapFilter from "@/components/filter";
 import MapContainer from "@/components/map/container";
-// import propertiesGeocoded from "@/data/properties-geocoded.jsonl";
-import fetchProperties from "@/hooks/fetchProperties";
+import useFetchProperties from "@/hooks/useFetchProperties";
 import { GeocodedProperty } from "@/types/Property";
 import { useState } from "react";
 
-import { useEffect } from "react";
-
 export default function FilteredMap() {
-    const [allProperties, setAllProperties] = useState<GeocodedProperty[]>([]);
-    const [properties, setProperties] = useState<GeocodedProperty[]>([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const propertiesGeocoded = await fetchProperties();
-            setAllProperties(propertiesGeocoded || []);
-        }
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        setProperties(allProperties);
-    },[allProperties]);
+    const allProperties = useFetchProperties();
+    const [properties, setProperties] = useState<GeocodedProperty[]>(allProperties);
 
     return (
         <>
