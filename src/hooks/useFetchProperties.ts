639b0cbe4c3148df3e78b8467315d90cc3a82231
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-10-22 17:48:17 -0300

    converts supabase request to hook

---
+++ src/hooks/useFetchProperties.ts
@@ -0,0 +1,37 @@
+"use client";
+import { GeocodedProperty } from "@/types/Property";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+export default function useFetchProperties(): GeocodedProperty[] {
+    const [cachedProperties, setCachedProperties] = useState<GeocodedProperty[]>([]);
+
+    useEffect(() => {
+        const supabase = createClient();
+        console.log("Fetching properties...");
+        async function fetchProperties() {
+            const { count: propertiesCount } = await supabase
+                .from("properties")
+                .select("caixaId", { count: "exact", head: true });
+
+            if (!propertiesCount) return [];
+
+            const allProperties: GeocodedProperty[] = [];
+
+            for (let i = 0; i < propertiesCount; i += 1000) {
+                const { data: propertiesPage } = await supabase
+                    .from("properties")
+                    .select()
+                    .range(i, i + 999);
+
+                if (!propertiesPage) return allProperties;
+
+                allProperties.push(...propertiesPage);
+            }
+            setCachedProperties(allProperties);
+        }
+        fetchProperties();
+        console.log("Properties fetched!");
+    }, []);
+    return cachedProperties;
+}
