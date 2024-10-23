d0ffa6136523a14de7f87a3741153e0ea34686c2
Pedro Ribeiro <pribeiro@extensiv.com>
2024-10-23 01:13:46 -0300

    fix masked input

--- src/components/ui/money-input.tsx
+++ src/components/ui/money-input.tsx
@@ -11,19 +11,19 @@ type TextInputProps = {
 // Brazilian currency config
 const moneyFormatter = Intl.NumberFormat("pt-BR", {
     currency: "BRL",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
 });
 
 export default function MoneyInput(props: TextInputProps) {
     const { onChange, value } = props;
 
     const digits = `${value}`.replace(/\D/g, "");
-    const maskedValue = moneyFormatter.format(Number(digits) / 100);
+    const maskedValue = moneyFormatter.format(Number(digits));
 
     function handleChange(formattedValue: string) {
         const digits = formattedValue.replace(/\D/g, "");
-        const realValue = Number(digits) / 100;
+        const realValue = Number(digits);
         onChange(realValue);
     }
 
