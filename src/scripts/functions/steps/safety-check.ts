d46941052d68e938def4fe33d597bd1b20a8456d
Pedro Ribeiro <pribeiro@extensiv.com>
2024-10-23 22:33:51 -0300

    adds safety check to resetProperties script

---
+++ src/scripts/functions/steps/safety-check.ts
@@ -0,0 +1,26 @@
+import readline from "readline";
+import "dotenv/config";
+
+function askQuestion(query: string): Promise<string> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise((resolve) =>
+        rl.question(query, (ans) => {
+            rl.close();
+            resolve(ans);
+        })
+    );
+}
+
+const safetyCheck = async () => {
+    const ans = await askQuestion("Are you sure you want to run in DELETE all properties? (Y/N): ");
+    if (!["y", "yes"].includes(ans.toLowerCase())) {
+        console.log("Script aborted");
+        process.abort();
+    }
+};
+
+export default safetyCheck;
--- src/scripts/run-reset-properties.ts
+++ src/scripts/run-reset-properties.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import resetProperties from "./functions/reset-properties";
 
 resetProperties().catch(console.error);
