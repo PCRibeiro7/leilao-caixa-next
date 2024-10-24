67e5fa187118fa1095ab08a7e27b2eaaf204469c
Pedro Ribeiro <pribeiro@extensiv.com>
2024-10-23 22:18:13 -0300

    changes temp folder name to enable write on cloud function

--- src/consts/filePaths.ts
+++ src/consts/filePaths.ts
@@ -1,3 +1,3 @@
-export const TMP_PATH = ".tmp";
+export const TMP_PATH = "tmp";
 export const PROPERTIES_PATH = `${TMP_PATH}/properties.jsonl`;
 export const PROPERTIES_RAW_PATH = `${TMP_PATH}/properties-raw.csv`;
