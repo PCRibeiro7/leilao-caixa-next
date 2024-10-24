6289d5581590d7d5b295d42ce26fe0971f41a55b
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-10-23 22:33:51 -0300

    adds safety check to resetProperties script

--- src/scripts/functions/reset-properties.ts
+++ src/scripts/functions/reset-properties.ts
@@ -3,8 +3,13 @@ import deleteAllProperties from "./steps/cleanup-geocoded-properties";
 import fetchRawProperties from "./steps/fetch-raw-properties";
 import parseProperties from "./steps/parse-properties";
 import fetchGeocodeData from "./steps/fetch-geocode-data";
+import safetyCheck from "./steps/safety-check";
 
-export default async function resetProperties() {
+
+export default async function resetProperties(shouldSafetyCheck = true) {
+    if (shouldSafetyCheck) {
+        await safetyCheck();
+    }
     cleanupProperties();
     await deleteAllProperties();
     await fetchRawProperties();
