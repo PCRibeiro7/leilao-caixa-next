a69471da767623494372a8be8dbbadd2cb03588d
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-10-23 21:36:48 -0300

    change cron

--- netlify/functions/update-properties-cron/update-properties-cron.ts
+++ netlify/functions/update-properties-cron/update-properties-cron.ts
@@ -3,7 +3,8 @@ import { schedule } from "@netlify/functions";
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-export const handler = schedule("*/5 * * * *", async (event) => {
+// export const handler = schedule("*/5 * * * *", async (event) => {
+export const handler = schedule("*/60 * * * *", async (event) => {
     const eventBody = JSON.parse(event.body || "{}");
     console.log(`Next function run at ${eventBody?.next_run}.`);
 
