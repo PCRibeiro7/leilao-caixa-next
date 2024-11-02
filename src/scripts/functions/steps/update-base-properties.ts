23b0c15ed291a9c37477565e6fcb2f6c536985cb
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2024-11-02 03:16:43 -0300

    Refactor photo update logic in setBaseProperties function; streamline missing photos handling and logging

--- src/scripts/functions/steps/update-base-properties.ts
+++ src/scripts/functions/steps/update-base-properties.ts
@@ -15,12 +15,12 @@ async function setBaseProperties(): Promise<void> {
     if (shouldUpdatePhotos) {
         const files = await listFiles();
         const fileNames = files?.map((file) => file.name.split(".")[0]);
+
+        const missingPhotosProperties = properties.filter((property) => !fileNames?.includes(property.caixaId));
         console.log(`Found ${fileNames?.length} files`);
 
-        for (const [index, property] of properties.entries()) {
-            if (index % 100 === 0) console.log(`Updating property ${index} of ${properties.length}`);
-
-            if (fileNames?.includes(property.caixaId)) continue;
+        for (const [index, property] of missingPhotosProperties.entries()) {
+            if (index % 100 === 0) console.log(`Updating photo ${index} of ${missingPhotosProperties.length}`);
 
             const base64 = await getImage(property.caixaId);
             if (!base64) continue;
