0e70e5b79349d9abb71a37d1ca15bccad5ec1ab4
Pedro Ribeiro <pedro.costa.ribeiro@gmail.com>
2026-03-04 03:05:18 -0300

    Add "Financiamento" to the list of property attributes in parseProperties function

--- src/scripts/functions/steps/parse-properties.ts
+++ src/scripts/functions/steps/parse-properties.ts
@@ -70,6 +70,7 @@ async function parseProperties(): Promise<void> {
                             "Preço",
                             "Valor de avaliação",
                             "Desconto",
+                            "Financiamento",
                             "Descrição",
                             "Modalidade de venda",
                             "Link de acesso",
